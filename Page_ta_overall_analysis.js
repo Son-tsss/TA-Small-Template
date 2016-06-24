f9beb01ce379e6fadfd372fa7adba915a10bbc9b
Son-tsss <marya-sokolova@yandex.ru>
2016-06-24 16:53:31 +0300

    work with categories parameters

--- Page_ta_overall_analysis.js
+++ Page_ta_overall_analysis.js
@@ -5,13 +5,45 @@ class Page_ta_overall_analysis {
     static function Render (context) {
         TALibrary.setReport(context.pageContext, context.log, context.report, context.confirmit, context.user);
         TALibrary.setCurrentQuestion(context.pageContext.Items["questionID"]);
-    if(context.component.SubmitSource=="btnResetCategories")
-    {
-        context.state.Parameters[TALibrary.currentQuestion.questionDetails.TACategoryListParameter]=null;
-        TALibrary.currentQuestion.setCurrentTheme(null);
-    }else{
-        TALibrary.currentQuestion.setCurrentTheme(context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TACategoryListParameter));
-    }
+        switch(context.component.SubmitSource){
+            case "btnResetAttributes":
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TAAttributesListParameter]=null;
+                TALibrary.currentQuestion.setCurrentAttribute(null);
+                break;
+            case "btnResetSubcategories":
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TAAttributesListParameter]=null;
+                TALibrary.currentQuestion.setCurrentAttribute(null);
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter]=null;
+                TALibrary.currentQuestion.setCurrentSubcategory(null);
+                break;
+            case "btnResetCategories":
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TAAttributesListParameter]=null;
+                TALibrary.currentQuestion.setCurrentAttribute(null);
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter]=null;
+                TALibrary.currentQuestion.setCurrentSubcategory(null);
+                context.state.Parameters[TALibrary.currentQuestion.questionDetails.TACategoryListParameter]=null;
+                TALibrary.currentQuestion.setCurrentTheme(null)
+                break;
+            default:
+                if(context.state.Parameters.IsNull(TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter) || TALibrary.currentQuestion.currentSubcategory<0 || context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter)!=TALibrary.currentQuestion.subcategories[TALibrary.currentQuestion.currentSubcategory].id){
+                    context.state.Parameters[TALibrary.currentQuestion.questionDetails.TAAttributesListParameter]=null;
+                    TALibrary.currentQuestion.setCurrentAttribute(null);
+                }else{
+                    TALibrary.currentQuestion.setCurrentAttribute(context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter));
+                }
+
+                if(context.state.Parameters.IsNull(TALibrary.currentQuestion.questionDetails.TACategoryListParameter) || TALibrary.currentQuestion.currentTheme<0 || context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TACategoryListParameter)!=TALibrary.currentQuestion.themes[TALibrary.currentQuestion.currentTheme].id){
+                    context.state.Parameters[TALibrary.currentQuestion.questionDetails.TAAttributesListParameter]=null;
+                    TALibrary.currentQuestion.setCurrentAttribute(null);
+                    context.state.Parameters[TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter]=null;
+                    TALibrary.currentQuestion.setCurrentSubcategory(null);
+                }else{
+                    TALibrary.currentQuestion.setCurrentSubcategory(context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TASubcategoryListParameter));
+                }
+
+                TALibrary.currentQuestion.setCurrentTheme(context.state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TACategoryListParameter));
+                break;
+        }
     }
 
     /*-------Tables-------*/
