9b43a0d709bf1791ed0950a607c4a99c9d0eccff
Son-tsss <marya-sokolova@yandex.ru>
2016-06-24 18:25:31 +0300

    distribution

--- TATableUtils.js
+++ TATableUtils.js
@@ -110,17 +110,17 @@ class TATableUtils{
             switch(groupName){
                 case "neg":
                     headerCategories.Mask.Codes = '1,2,3,4,5';
-                    headerFormula.Expression = "(cellv(col-5,row)+cellv(col-4,row)+cellv(col-3,row)+cellv(col-2,row)+cellv(col-1,row))"+(addMinus?"*(-1)":"")+(distribution == "distr1"?"/100":"");
+                    headerFormula.Expression = "(cellv(col-5,row)+cellv(col-4,row)+cellv(col-3,row)+cellv(col-2,row)+cellv(col-1,row))"+(addMinus?"*(-1)":"")+((distribution == "distr1")?("/100"):"");
                     categoryTitle = new Label(9, "Negative");
                     break;
                 case "neu":
                     headerCategories.Mask.Codes = '6';
-                    headerFormula.Expression = "cellv(col-1,row)"+(distribution == "distr1"?"/100":"");
+                    headerFormula.Expression = "(cellv(col-1,row))"+((distribution == "distr1")?("/100"):"");
                     categoryTitle = new Label(9, "Neutral");
                     break;
                 case "pos":
                     headerCategories.Mask.Codes = '7,8,9,10,11';
-                    headerFormula.Expression = "cellv(col-5,row)+cellv(col-4,row)+cellv(col-3,row)+cellv(col-2,row)+cellv(col-1,row)"+(distribution == "distr1"?"/100":"");
+                    headerFormula.Expression = "(cellv(col-5,row)+cellv(col-4,row)+cellv(col-3,row)+cellv(col-2,row)+cellv(col-1,row))"+((distribution == "distr1")?("/100"):"");
                     categoryTitle = new Label(9, "Positive");
                     break;
             }
