8f3a2476ce0d6bfb51e52b219a140227fb1f7fd1
annyrtd <fyz95@mail.ru>
2016-08-04 14:37:03 +0300

    Debugging

--- Page_ta_detailed_analysis.js
+++ Page_ta_detailed_analysis.js
@@ -152,7 +152,7 @@ class Page_ta_detailed_analysis {
 				}
 				if (!f1 && !f3) {
 					log.LogDebug('3: ' + state.Parameters.GetString(TALibrary.currentQuestion.questionDetails.TACategoryListParameter)!=TALibrary.currentQuestion.themes[TALibrary.currentQuestion.currentTheme].id);
-				}
+				} 
 				log.LogDebug('ENDING');
 				
                 if(state.Parameters.IsNull(TALibrary.currentQuestion.questionDetails.TACategoryListParameter) 
