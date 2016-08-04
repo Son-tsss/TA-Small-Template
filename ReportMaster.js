0aec433529b1065d5175ea5653c7fd6469e52858
annyrtd <fyz95@mail.ru>
2016-08-04 16:18:49 +0300

    series width fix

--- ReportMaster.js
+++ ReportMaster.js
@@ -503,9 +503,9 @@ class ReportMaster {
 		"            chartOptions.plotOptions.series.dataLabels.style.color = \"" + TAConfig.Design.DarkGrey + "\"; /*add color*/" +
 		"            console.log(chartOptions.plotOptions.series);" +
 		"            if (chartOptions.serier.length < 5) {" +
-		"            	chartOptions.plotOptions.series.maxPointWidth = 60;" +
-		"            	chartOptions.plotOptions.column.maxPointWidth = 60;" +
-		"            	chartOptions.plotOptions.bar.maxPointWidth = 60;" +
+		"            	chartOptions.plotOptions.series.pointWidth = 60;" +
+		"            	chartOptions.plotOptions.column.pointWidth = 60;" +
+		"            	chartOptions.plotOptions.bar.pointWidth = 60;" +
 		"            }"		
 		"        });" +
 		"    });"+
